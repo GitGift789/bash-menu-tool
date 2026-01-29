46d8e719c8d378f43ab60ec46eeb9d202f2168e5
GitGift789 <lebronronaldo152@gmail.com>
2026-01-29 12:51:31 -0600

    Add greet_user function with input

--- toolkit.sh
+++ toolkit.sh
@@ -8,3 +8,9 @@ LOG_FILE="activity.log"   #Stores the name of the log file
 echo "Bash Toolkit Starting"
 #This prints a welcome message
 
+# FUNCTION: GREET THE USER
+greet_user() {
+    read -p "Enter your name: " USER_NAME #Asks the user for their name
+    echo "Welcome, $USER_NAME"            #Greets the user  
+}
+greet_user
