d0894cee3c726060fec97cb37873c52c8f5c9258
GitGift789 <lebronronaldo152@gmail.com>
2026-01-29 12:41:46 -0600

    Add variables for user name and log file

--- toolkit.sh
+++ toolkit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #This tells the computer to run this script using bash
 
+#VARIABLES
+USER_NAME=""              #Stores the user's name
+LOG_FILE="activity.log"   #Stores the name of the log file
+
 echo "Bash Toolkit Starting"
-#this prints a welcome message
+#This prints a welcome message
+
